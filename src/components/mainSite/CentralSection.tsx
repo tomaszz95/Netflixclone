3a20fdebd4f04d63504ee38475ade668bdfda016
Tomasz Ż <tzuber95@gmail.com>
2023-09-28 18:58:39 +0200

    main section end

---
+++ src/components/mainSite/CentralSection.tsx
@@ -0,0 +1,78 @@
+import styles from './CentralSection.module.css'
+import { sectionMainDataObj } from '../helpers/types'
+
+const sectionData: sectionMainDataObj = {
+    '1': {
+        title: 'Enjoy on your TV',
+        text: 'Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.',
+        image: '/photos/tv.png',
+        video: 'https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v',
+    },
+    '2': {
+        title: 'Download your shows to watch offline',
+        text: 'Save your favorites easily and always have something to watch.',
+        image: '/photos/mobile.jpg',
+    },
+    '3': {
+        title: 'Watch everywhere',
+        text: 'Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.',
+        image: '/photos/device.png',
+        video: 'https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices.m4v',
+    },
+    '4': {
+        title: 'Create profiles for kids',
+        text: 'Send kids on adventures with their favorite characters in a space mode just for them - free with your membership.',
+        image: '/photos/kids.png',
+    },
+}
+
+type ComponentType = { sectionCount: string }
+
+const CentralSection: React.FC<ComponentType> = ({ sectionCount }) => {
+    const section = sectionData[sectionCount]
+    let insideBox
+
+    if (sectionCount === '1') {
+        insideBox = (
+            <div className={styles.video}>
+                <video src={section.video} autoPlay playsInline muted loop></video>
+            </div>
+        )
+    } else if (sectionCount === '2') {
+        insideBox = (
+            <div className={styles.stranger}>
+                <div className={styles.strangerBox}>
+                    <img src="/photos/stranger.png" alt="Stranger things movie screen" />
+                    <div className={styles.strangerText}>
+                        <p>Stranger Things</p>
+                        <span>Downloading...</span>
+                    </div>
+                </div>
+                <div className={styles.animation}></div>
+            </div>
+        )
+    } else if (sectionCount === '3') {
+        insideBox = (
+            <div className={styles.videoSecond}>
+                <video src={section.video} autoPlay playsInline muted loop></video>
+            </div>
+        )
+    } else if (sectionCount === '4') {
+        insideBox = <div></div>
+    }
+
+    return (
+        <section className={styles.section}>
+            <div className={styles.sectionBox}>
+                <h2>{section.title}</h2>
+                <p>{section.text}</p>
+            </div>
+            <div className={styles.imageBox}>
+                <img src={section.image} alt={section.title} className={styles.image} />
+                {insideBox}
+            </div>
+        </section>
+    )
+}
+
+export default CentralSection
--- src/components/mainSite/MainView.tsx
+++ src/components/mainSite/MainView.tsx
@@ -1,3 +1,4 @@
+import CentralSection from './CentralSection'
 import HeaderSection from './HeaderSection'
 import HeroSection from './HeroSection'
 
@@ -5,10 +6,18 @@ import styles from './MainView.module.css'
 
 const MainView = () => {
     return (
-        <main className={styles.main}>
-            <HeaderSection />
-            <HeroSection />
-        </main>
+        <>
+            <header className={styles.header}>
+                <HeaderSection />
+                <HeroSection />
+            </header>
+            <main className={styles.main}>
+                <CentralSection sectionCount="1" />
+                <CentralSection sectionCount="2" />
+                <CentralSection sectionCount="3" />
+                <CentralSection sectionCount="4" />
+            </main>
+        </>
     )
 }
 
