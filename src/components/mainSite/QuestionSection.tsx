1b24751c182f9c607d89277e0fc7b1fae7aa42a7
Tomasz Ż <tzuber95@gmail.com>
2023-10-04 14:22:10 +0200

    end questions section

--- src/components/mainSite/QuestionSection.tsx
+++ src/components/mainSite/QuestionSection.tsx
@@ -1,24 +1,14 @@
+import MembershipEmail from './MembershipEmail'
 import QuestionsAccordion from './QuestionsAccordion'
+
 import styles from './QuestionSection.module.css'
 
 const QuestionSection = () => {
     return (
-        <section>
-            <h2>Frequently Asked Questions</h2>
+        <section className={styles.questions}>
+            <h2 className={styles.questionsTitle}>Frequently Asked Questions</h2>
             <QuestionsAccordion />
-            <div className={styles.heroContent}>
-                <div className={styles.heroText}>
-                    <h1>The biggest local and international hits. The best stories. All streaming here.</h1>
-                    <p>Watch anywhere. Cancel anytime.</p>
-                    <p>Ready to watch? Enter your email to create or restart your membership.</p>
-                </div>
-                <div className={styles.heroStart}>
-                    <input type="email" placeholder="Email address" autoComplete="email" minLength={5} maxLength={50} />
-                    <button type="button" aria-label="Get started button">
-                        Get Started &gt;
-                    </button>
-                </div>
-            </div>
+            <MembershipEmail />
         </section>
     )
 }
--- src/components/mainSite/QuestionsAccordion.tsx
+++ src/components/mainSite/QuestionsAccordion.tsx
@@ -1,5 +1,136 @@
+import { useState } from 'react'
+
 import styles from './QuestionsAccordion.module.css'
 
-const QuestionsAccordion = () => {}
+const QuestionsAccordion = () => {
+    const [activeAccordion, setActiveAccordion] = useState(0)
+
+    const openCookieText = (index: number) => {
+        if (activeAccordion === index) {
+            setActiveAccordion(index)
+        } else {
+            setActiveAccordion(index)
+        }
+    }
+    return (
+        <div className={styles.accordion}>
+            <div className={styles.accordionBox}>
+                <button
+                    className={`${styles.accordionItem} ${activeAccordion === 0 ? 'openQuestionText' : ''}`}
+                    onClick={() => openCookieText(0)}
+                >
+                    What is Netflix?
+                </button>
+                <div className={`${styles.accordionText} ${activeAccordion === 0 ? 'openQuestionText' : ''}`}>
+                    <p>
+                        <span>
+                            Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies,
+                            anime, documentaries, and more on thousands of internet-connected devices.
+                        </span>
+                        <br />
+                        <span>
+                            You can watch as much as you want, whenever you want without a single commercial – all for
+                            one low monthly price. There's always something new to discover and new TV shows and movies
+                            are added every week!
+                        </span>
+                    </p>
+                </div>
+            </div>
+
+            <div className={styles.accordionBox}>
+                <button
+                    className={`${styles.accordionItem} ${activeAccordion === 0 ? 'openQuestionText' : ''}`}
+                    onClick={() => openCookieText(1)}
+                >
+                    How much does Netflix cost?
+                </button>
+                <div className={`${styles.accordionText} ${activeAccordion === 1 ? 'openQuestionText' : ''}`}>
+                    <p>
+                        Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one
+                        fixed monthly fee. Plans range from 29 zł to 60 zł a month. No extra costs, no contracts.
+                    </p>
+                </div>
+            </div>
+
+            <div className={styles.accordionBox}>
+                <button
+                    className={`${styles.accordionItem} ${activeAccordion === 0 ? 'openQuestionText' : ''}`}
+                    onClick={() => openCookieText(2)}
+                >
+                    Where can i watch?
+                </button>
+                <div className={`${styles.accordionText} ${activeAccordion === 2 ? 'openQuestionText' : ''}`}>
+                    <p>
+                        <span>
+                            Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at
+                            netflix.com from your personal computer or on any internet-connected device that offers the
+                            Netflix app, including smart TVs, smartphones, tablets, streaming media players and game
+                            consoles.
+                        </span>
+                        <br />
+                        <span>
+                            You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use
+                            downloads to watch while you're on the go and without an internet connection. Take Netflix
+                            with you anywhere.
+                        </span>
+                    </p>
+                </div>
+            </div>
+
+            <div className={styles.accordionBox}>
+                <button
+                    className={`${styles.accordionItem} ${activeAccordion === 0 ? 'openQuestionText' : ''}`}
+                    onClick={() => openCookieText(3)}
+                >
+                    How do I cancel?
+                </button>
+                <div className={`${styles.accordionText} ${activeAccordion === 3 ? 'openQuestionText' : ''}`}>
+                    <p>
+                        Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your
+                        account online in two clicks. There are no cancellation fees – start or stop your account
+                        anytime.
+                    </p>
+                </div>
+            </div>
+
+            <div className={styles.accordionBox}>
+                <button
+                    className={`${styles.accordionItem} ${activeAccordion === 0 ? 'openQuestionText' : ''}`}
+                    onClick={() => openCookieText(4)}
+                >
+                    What can I watch on Netflix?
+                </button>
+                <div className={`${styles.accordionText} ${activeAccordion === 4 ? 'openQuestionText' : ''}`}>
+                    <p>
+                        Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning
+                        Netflix originals, and more. Watch as much as you want, anytime you want.
+                    </p>
+                </div>
+            </div>
+
+            <div className={styles.accordionBox}>
+                <button
+                    className={`${styles.accordionItem} ${activeAccordion === 0 ? 'openQuestionText' : ''}`}
+                    onClick={() => openCookieText(5)}
+                >
+                    Is Netflix good for kids?
+                </button>
+                <div className={`${styles.accordionText} ${activeAccordion === 5 ? 'openQuestionText' : ''}`}>
+                    <p>
+                        <span>
+                            The Netflix Kids experience is included in your membership to give parents control while
+                            kids enjoy family-friendly TV shows and movies in their own space.
+                        </span>
+                        <br />
+                        <span>
+                            Kids profiles come with PIN-protected parental controls that let you restrict the maturity
+                            rating of content kids can watch and block specific titles you don’t want kids to see.
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default QuestionsAccordion
