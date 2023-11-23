7830425cd9bb9d4031da079f4909ccaddfba2452
Tomasz Ż <tzuber95@gmail.com>
2023-11-23 18:03:17 +0100

    Create hero section styles

--- src/pages/browse/index.tsx
+++ src/pages/browse/index.tsx
@@ -1,7 +1,14 @@
+import { useEffect, useState } from 'react'
+import { fetchedMainWholeObj } from '../../components/helpers/types'
+
 import ReactProviderCookiesData from '../../components/helpers/ReactProviderCookiesData'
 import LoggedMainSiteView from '../../components/loggedMainSite/LoggedMainSiteView'
+import { getAllAPIFetchedData } from '../api/mainSite'
 
-const BrowsePage = () => {
+const BrowsePage: React.FC<fetchedMainWholeObj> = ({ fetchedData }) => {
+    const [fetchedContentObjects, setfetchedContentObjects] = useState([])
+
+    console.log(fetchedData)
     return (
         <ReactProviderCookiesData>
             <LoggedMainSiteView />
@@ -10,3 +17,13 @@ const BrowsePage = () => {
 }
 
 export default BrowsePage
+
+export async function getStaticProps() {
+    const fetchedData = await getAllAPIFetchedData()
+
+    return {
+        props: {
+            fetchedData,
+        },
+    }
+}
