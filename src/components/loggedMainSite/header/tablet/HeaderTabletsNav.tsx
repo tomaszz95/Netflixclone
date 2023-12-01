400f92747c7b17c304f96260389918157f38348c
Tomasz Ż <tzuber95@gmail.com>
2023-12-01 15:29:20 +0100

    start account site

--- src/components/loggedMainSite/header/tablet/HeaderTabletsNav.tsx
+++ src/components/loggedMainSite/header/tablet/HeaderTabletsNav.tsx
@@ -1,11 +1,5 @@
 import Link from 'next/link'
-import { useDispatch } from 'react-redux'
-import { ThunkDispatch } from '@reduxjs/toolkit'
 
-import auth from '../../../../../firebase'
-import { signOut } from 'firebase/auth'
-import { isLoggedInActions } from '../../../store/loggedin'
-import { deleteCookie } from '../../../helpers/localStorageFunctions'
 import { navigationBrowseLinks, navigationKidsLinks } from '../../../helpers/siteText'
 
 import styles from './HeaderTabletsNav.module.css'
@@ -17,18 +11,6 @@ type ComponentType = {
 }
 
 const HeaderTabletsNav: React.FC<ComponentType> = ({ chosenUser, onOpenNav, navActive }) => {
-    const dispatch = useDispatch<ThunkDispatch<any, any, any>>()
-
-    const logoutHandler = () => {
-        signOut(auth)
-
-        dispatch(isLoggedInActions.createLoggedCookie('false'))
-        deleteCookie('chosenUser')
-        deleteCookie('signInEmail')
-        deleteCookie('signUpEmail')
-        deleteCookie('startSignUpEmail')
-    }
-
     return (
         <>
             <button
