070b8727de95ad34da8cca769740db63c5190a84
Tomasz Ż <tzuber95@gmail.com>
2024-01-24 19:57:50 +0100

    improve register and simple setup code

--- src/components/simpleSetup/DeviceSurveyInput.tsx
+++ src/components/simpleSetup/DeviceSurveyInput.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
+import CHECK_ICON from '../../assets/icons/checkIconFull.png'
 import styles from './DeviceSurveyInput.module.css'
 
 type ComponentType = {
-    icon: string
+    icon: any
     id: string
     title: string
     text: string
@@ -24,7 +26,7 @@ const DeviceSurveyInput: React.FC<ComponentType> = ({ id, icon, title, text, che
 
     return (
         <label htmlFor={id} className={`${styles.label} ${isChecked ? styles.chosen : ''}`}>
-            <img src="/icons/checkIconFull.png" alt="" className={styles.checkIcon} />
+            <Image src={CHECK_ICON} alt="" className={styles.checkIcon} />
             <input
                 type="checkbox"
                 id={id}
@@ -34,7 +36,7 @@ const DeviceSurveyInput: React.FC<ComponentType> = ({ id, icon, title, text, che
             />
             {icon !== '' && (
                 <div className={styles.iconBox}>
-                    <img src={icon} alt="" className={styles.icon} />
+                    <Image src={icon} alt="" className={styles.icon} width={100} height={100} />
                 </div>
             )}
             <div className={styles.textBox}>
