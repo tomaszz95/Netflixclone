18e0aec53e44b616f7fdb5758c15bb361a3bf359
Tomasz Ż <tzuber95@gmail.com>
2024-01-27 13:25:39 +0100

    create custom window width hook

---
+++ src/hooks/useWindowWidth.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+
+function useWindowWidth() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth)
+        }
+
+        window.addEventListener('resize', handleResize)
+        
+        handleResize()
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
+    return windowWidth
+}
+
+export default useWindowWidth
